df7c8c6b6b82454c52fb05e95272468bc403a127
Carlos Marti <carlos199m1@gmail.com>
2023-10-23 13:54:52 -0400

    r2 script updated

--- 3_calc_r2s.sh
+++ 3_calc_r2s.sh
@@ -3,4 +3,8 @@ dataset="gb1"
 activate="conda activate gpmap"
 submit="qsub -cwd -l mem_free=16G"
 
-echo "$activate & evaluate_split_fits $dataset.csv -p $dataset -o $dataset.r2.csv" | $submit -N "r2"
+for dataset in $(grep -v '^#' datasets.txt)  # control which datasets are run through this file
+do
+	echo "$activate & evaluate_split_fits datasets/$dataset.csv -p output/$dataset -o $dataset.r2.csv" | $submit -N "r2.$dataset"
+
+done
