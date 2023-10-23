86a5ab056b3ac8c2e739720b69242f8f5868963c
Carlos Marti <carlos199m1@gmail.com>
2023-10-23 13:33:31 -0400

    script for submission updated

--- 2_fit_models.sh
+++ 2_fit_models.sh
@@ -8,16 +8,20 @@ gpu_options="--gpu -m $ngpu"                 # -s 1000 for partitioning
 for dataset in $(grep -v '^#' datasets.txt)  # control which datasets are run through this file
 do
 
-for i in $(seq 2 5)
+for i in $(seq 0 49)
 do
 	trainx="splits/$dataset.$i.train.csv"
 	testx="splits/$dataset.$i.test.txt"
 	cmd="$activate; $epik $trainx -p $testx"
 
-	for kernel in Rho RhoPi # VC DP Rho RhoPi RBF ARD
+	for kernel in Rho RhoPi  VC DP Rho RhoPi RBF ARD
 	do	
 		jid="$kernel.$i.$dataset"
-		echo "$cmd -k $kernel -o $out_dir/$dataset.$i.$kernel.test_pred.csv $gpu_options" | $submit -N "$jid" -e "logs/$jid.err" -o "logs/$jid.out"
+		run="$cmd -k $kernel -o $out_dir/$dataset.$i.$kernel.test_pred.csv $gpu_options"
+		echo "$run" | $submit -N "$jid" -e "logs/$jid.err" -o "logs/$jid.out" # Comment before PIPE to only show the command
+
+		# Uncomment next line to run in command line directly
+		# $run  #
 	done
 done
 
